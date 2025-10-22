30f73d70e7c3ed9e1675fb211b76a873eb352e55
mohammad <mh177148@gmail.com>
2025-10-22 13:31:03 +0330

    project express

---
+++ controllers/shop.js
@@ -0,0 +1,72 @@
+
+const Product = require('../models/product');
+
+
+exports.getProducts = (req, res, next) => {
+
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products',
+        });
+    });
+};
+
+
+
+exports.getProduct = (req , res , next ) => {
+    const prodId = req.params.productId
+
+    Product.findById(prodId , product => {
+        console.log(product , 'product');
+        
+    })
+
+    res.redirect('/')
+    
+}
+
+
+exports.getIndex = (req, res, next) => {
+
+    Product.fetchAll(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+        });
+    });
+
+}
+
+
+exports.getCart = (req, res, next) => {
+
+        res.render('shop/cart', {
+            pageTitle: 'Your Cart',
+            path: '/cart',
+        });
+    
+}
+
+
+exports.getOrders = (req, res, next) => {
+
+        res.render('shop/orders', {
+            pageTitle: 'Your Orders',
+            path: '/orders',
+        });
+    
+}
+
+
+
+exports.getCheckout = (req , res , next) => {
+
+    res.render('shop/checkout' ,  {
+        path:'/checkout' ,
+        pageTitle:'Checkout'
+    })
+    
+}
