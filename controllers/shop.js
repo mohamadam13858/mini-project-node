af2660dc04c656454c6c4c42677336dc60650f6c
mohammad <mh177148@gmail.com>
2025-10-23 10:32:30 +0330

    add-to-cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,6 +48,14 @@ exports.getCart = (req, res, next) => {
 }
 
 
+exports.postCart = (req , res , next ) => {
+    const prodId = req.body.productId
+    
+        
+    res.redirect('/cart')
+}
+
+
 exports.getOrders = (req, res, next) => {
 
         res.render('shop/orders', {
