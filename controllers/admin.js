ed0b2013246f892d2232294a3e77c96b1e9dfbff
mohammad <mh177148@gmail.com>
2025-10-28 15:07:01 +0330

    edit-product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,15 +14,13 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl 
     const price = req.body.price
     const description = req.body.description
-    const product = new Product(title , imageUrl , description , price);
+    const product = new Product(null ,title , imageUrl , description , price);
     product.save();
     res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit
-
-    console.log(editMode , 'edit Mode ');
     
     if (!editMode) {
         return res.redirect('/')
@@ -43,7 +41,15 @@ exports.getEditProduct = (req, res, next) => {
 
 
 exports.postEditProduct = (req ,res , next) => {
-    
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDescription = req.body.description
+  const updatedProduct = new Product(prodId, updatedTitle ,updatedImageUrl , updatedDescription , updatedPrice)
+  console.log(prodId , 'prodId');
+  
+  updatedProduct.save()
 }
 
 
--- models/product.js
+++ models/product.js
@@ -18,20 +18,30 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title , imageUrl, description , price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id
     this.title = title;
     this.imageUrl = imageUrl;
-    this.description = description ;
+    this.description = description;
     this.price = price
   }
 
   save() {
-    this.id = Math.random().toString()
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+        const updatedProduct = [...products]
+        updatedProduct[existingProductIndex] = this
+        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString()
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -39,7 +49,7 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }
 
-  static findById (id , cb) {
+  static findById(id, cb) {
     getProductsFromFile(products => {
       const product = products.find(p => p.id === id)
       cb(product)
