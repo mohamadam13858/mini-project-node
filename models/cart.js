6da18bca70136a691228f4a80634a918bd1919fa
mohammad <mh177148@gmail.com>
2025-10-30 11:52:34 +0330

    deleteProduct

--- models/cart.js
+++ models/cart.js
@@ -51,11 +51,8 @@ module.exports = class Cart {
             const updatedCart = { ...JSON.parse(fileContent)     }
             const product = updatedCart.products.findIndex(prod => prod.id === id)
             const productQty = product.qty
-
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
-
             updatedCart.totalPrice = cart.totalPrice - productPrice * productQty
-
             fs.writeFile(p, JSON.stringify(updatedCart), err => {
                 console.log(err)
             })
