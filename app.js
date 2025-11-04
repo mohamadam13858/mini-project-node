4d1b409245783b1c85e2bbc4c672ec7ba6c4539c
mohammad <mh177148@gmail.com>
2025-11-04 11:45:59 +0330

    db

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require('path');
-
+const db = require('./util/database')
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
 const app = express();
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
