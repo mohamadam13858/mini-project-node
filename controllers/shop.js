2bb18da2846771668084bf11f94f7ccbc564e6ba
mohammad <mh177148@gmail.com>
2025-11-05 12:20:09 +0330

    getbyId-getbyPk

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,8 +16,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId
-    Product.findById(prodId).then(([product]) => {
-        res.render('shop/product-detail', { product: product[0], pageTitle: product.title, path: '/products' })
+    Product.findByPk(prodId).then(product => {
+        res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products'})
     }).catch(err => console.log(err))
 }
 
