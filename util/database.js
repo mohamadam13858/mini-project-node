5e526d03c0a9a8f4453aaef06c228be4e5251eb1
mohammad <mh177148@gmail.com>
2025-11-05 11:57:23 +0330

    sequelize-create-get

--- util/database.js
+++ util/database.js
@@ -1,14 +1,11 @@
-const mysql = require('mysql2')
+const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'localhost',
+  database: 'node-complete',
+  username: 'root',
+  password: '1385',
+});
 
-const pool = mysql.createPool({
-    host: "localhost" ,
-    user: 'root' , 
-    port: 3306,
-    database: 'node-complete' ,
-    password: '1385'
-})
-
-
-
-module.exports = pool.promise()
+module.exports = sequelize;
