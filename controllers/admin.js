fa541e1b6f7b3a1650b9e50c171c22c10ac338a9
mohammad <mh177148@gmail.com>
2025-11-05 12:47:23 +0330

    delete-Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,17 +62,18 @@ exports.postEditProduct = (req, res, next) => {
         console.log('updated product')
         res.redirect('/admin/products')
     }).catch(err => console.log(err));
-
-
-
 }
 
 
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId
-    Product.deleteById(prodId)
-    res.redirect('/admin/products')
+    Product.findByPk(prodId).then(product => {
+        return  product.destroy()
+    }).then(result => {
+        console.log('delete product')
+        res.redirect('/admin/products')
+    }).catch(err => console.log(err));
 }
 
 
