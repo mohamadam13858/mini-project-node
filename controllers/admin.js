a519e04c5cf0dc8b8e22b99a78f7fd4331fc5e6a
mohammad <mh177148@gmail.com>
2025-11-12 14:44:12 +0330

    getProducts , createProduct , method

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,8 +37,8 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   req.user
-    // .getProducts({ where: { id: prodId } })
-    Product.findByPk(prodId)
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
     .then(products => {
       const product = products[0];
       if (!product) {
