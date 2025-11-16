cdc9fde9e0cfebd5ea18ba87cebb50e6fc470485
mohammad <mh177148@gmail.com>
2025-11-16 10:12:49 +0330

    cart,cartItem

--- models/cart-item.js
+++ models/cart-item.js
@@ -2,14 +2,16 @@ const {Sequelize} = require('sequelize')
 const sequelize = require('../util/database')
 
 
-const Cart = sequelize.define('cart' , {
+const CartItem = sequelize.define('cartItems' , {
     id: {
         type: Sequelize.INTEGER  ,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true,    
-    }
+        autoIncrement: true,
+    } ,
+    quantity : Sequelize.INTEGER ,
+
 } )
 
 
-module.exports = Cart;
+module.exports = CartItem;
