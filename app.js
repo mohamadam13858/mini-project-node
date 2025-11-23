38956cac8efb502dd4429b7fb7455af9e519e0a2
mohammad <mh177148@gmail.com>
2025-11-23 14:11:03 +0330

    post-orders

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ Cart.belongsTo(User)
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 Order.belongsTo(User)
-User.hasMany(User)
+User.hasMany(Order)
 Order.belongsToMany(Product, { through: OrderItem })
 
 sequelize
