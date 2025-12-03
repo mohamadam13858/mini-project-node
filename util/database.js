88c1cbcbeaa4597c8b5f8234235886b768f2ef19
mohammad <mh177148@gmail.com>
2025-12-03 14:14:23 +0330

    add-product-with-mongoDb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,33 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
 
-const sequelize = new Sequelize('node-complete', 'root', "1385", {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient
 
-module.exports = sequelize;
+
+let _db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://mh711748_db_user:13851358@cluster0.kozx12z.mongodb.net/?appName=Cluster0')
+        .then((client) => {
+            console.log('connect');
+            _db = client.db()
+            callback()
+        }).catch((err) => {
+            console.log('err');
+            console.log(err);
+            throw err
+        })
+
+
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+
+    throw 'not found database'
+}
+
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
