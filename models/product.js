8dcf8efe8931e2e2e1546015ae80a5ace5bb1bce
mohammad <mh177148@gmail.com>
2025-12-07 10:14:28 +0330

    getProduct

--- models/product.js
+++ models/product.js
@@ -13,11 +13,25 @@ class Product {
 
   save() {
     const db = getDb()
-    return  db.collection('products').insertOne(this)
-    .then(result => {
-      console.log(result , 'result')
-    })
-    .catch(err => console.log(err))
+    return db.collection('products').insertOne(this)
+      .then(result => {
+        console.log(result, 'result')
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
+  static fetchAll() {
+    const db = getDb()  
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products, 'products')
+        return products
+      })
+      .catch(err => console.log(err))
   }
 
 
