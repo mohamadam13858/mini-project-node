2f6f824d54863748dc13107dc4874f8def8fabad
mohammad <mh177148@gmail.com>
2025-12-09 14:58:57 +0330

    delete-cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -75,38 +75,11 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId).then(product => {
     return req.user.addToCart(product)
   })
-    .then(result => console.log(result))
+    .then(result => {
+      console.log(result)
+      res.redirect('/cart');
+    })
     .catch(err => console.log(err))
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItems.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 
@@ -114,20 +87,14 @@ exports.postCart = (req, res, next) => {
 
 
 
-// exports.postCartDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   req.user.getCart().then(cart => {
-//     return cart.getProducts({ where: { id: prodId } })
-//   })
-//     .then(products => {
-//       const product = products[0]
-//       return product.cartItems.destroy()
-//     })
-//     .then(result => {
-//       res.redirect('/cart');
-//     })
-//     .catch(err => console.log(err))
-// };
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.deleteItemFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err))
+};
 
 
 
