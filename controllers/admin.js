ea3a105ed89fe037e39c4be21cdf92265f6375e3
mohammad <mh177148@gmail.com>
2025-12-13 11:58:50 +0330

    mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,14 +14,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl , null , req.user._id)
+  const product = new Product({ title: title, price: price, description: description, imageUrl: imageUrl })
   product.save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
-    .catch(err => {
+    .catch(err => { 
       console.log(err);
     });
 };
@@ -54,8 +54,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc , updatedImageUrl,   prodId)
-  product.save().then(result => {
+   Product.findById(prodId).then(product => { 
+
+    product.title = updatedTitle 
+    product.price = updatedPrice 
+    product.description = updatedDesc 
+    product.imageUrl = updatedImageUrl
+      return product.save()
+
+   })
+  .then(result => {
     console.log('UPDATED PRODUCT!');
     res.redirect('/admin/products');
   })
@@ -63,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find() 
     .then(products => {
       res.render('admin/products', {
         prods: products,
