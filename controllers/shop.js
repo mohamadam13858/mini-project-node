67aee5ec3b58dad1c42d6496b829489a819839e5
mohammad <mh177148@gmail.com>
2025-12-13 17:48:02 +0330

    postOrder

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order')
 
 
 exports.getProducts = (req, res, next) => {
@@ -42,7 +43,7 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/' 
+        path: '/'
       });
     })
     .catch(err => {
@@ -57,15 +58,14 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.populate('cart.items.productId')
-  .then(user => {
-  
-    const products = user.cart.items
-    res.render('shop/cart', {
-      path: '/cart',
-      pageTitle: 'Your Cart',
-      products: products
-    });
-  }).catch(err => console.log(err))
+    .then(user => {
+      const products = user.cart.items
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    }).catch(err => console.log(err))
 };
 
 
@@ -92,7 +92,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)
+  req.user.removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -107,9 +107,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart
-  req.user.addOrder()
-    .then(result => {
+  req.user.populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
+      })
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save()
+    }).then(result => {
+      return req.user.clearCart()
+    }).then(() => {
       res.redirect('/orders')
     })
     .catch(err => console.log(err))
