0beb89e5268226d6621c9f494363dbe29ec9bed2
mohammad <mh177148@gmail.com>
2025-12-15 23:11:22 +0330

    connect-flash-error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
 const mongoose = require('mongoose')
 const csrf = require('csurf')
+const flash = require('connect-flash')
 
 const User = require('./models/user')
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: "my mohamad", resave: false, saveUninitialized: false, store: store }))
 
 app.use(csrfProtection)
+app.use(flash())
 
 app.use((req, res, next) => {
     if (!req.session.user) {
