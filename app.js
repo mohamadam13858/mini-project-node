27a965afd754d320b8af70540b33cdd9b60c84bf
mohammad <mh177148@gmail.com>
2025-12-18 11:48:13 +0330

    error500

--- app.js
+++ app.js
@@ -42,10 +42,15 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next()
+            }
             req.user = user;
             next();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            throw new Error(err)
+        });
 });
 
 
@@ -58,6 +63,9 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes)
+
+
+app.get('/500' , errorController.get500)
 
 app.use(errorController.get404);
 
