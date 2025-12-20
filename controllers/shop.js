eaad4cfdeaf26cf44dcbb1cca72f11ace2bdfcae
mohammad <mh177148@gmail.com>
2025-12-21 00:01:53 +0330

    reed

--- controllers/shop.js
+++ controllers/shop.js
@@ -147,21 +147,25 @@ exports.getOrders = (req, res, next) => {
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId
   Order.findById(orderId).then(order => {
-     if (!order) {
+    if (!order) {
       return next(new Error('no order found.'))
-     }
-     if (order.user.userId.toString() !== req.user._id.toString() ) {
-       return next(new Error('Unauthorized'))
-     }
-     const invoiceName = 'invoice-' + orderId + '.pdf'
-     const invoicePath = path.join('data', 'invoices', invoiceName)
-     fs.readFile(invoicePath, (err, data) => {
-       if (err) {
-         return next(err)
-       }
-       res.setHeader('Content-Type', 'application/pdf');
-       res.setHeader('Content-Disposition', 'inline');
-       res.send(data)
-     })
+    }
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error('Unauthorized'))
+    }
+    const invoiceName = 'invoice-' + orderId + '.pdf'
+    const invoicePath = path.join('data', 'invoices', invoiceName)
+    //  fs.readFile(invoicePath, (err, data) => {
+    //    if (err) {
+    //      return next(err)
+    //    }
+    //    res.setHeader('Content-Type', 'application/pdf');
+    //    res.setHeader('Content-Disposition', 'inline');
+    //    res.send(data)
+    //  })
+    const file = fs.createReadStream(invoicePath)
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline');
+    file.pipe(res)
   })
 }
