8948225cfbc271ff2539a8e917935602bd1168b9
mohammad <mh177148@gmail.com>
2025-12-21 14:23:44 +0330

    invoices

--- controllers/shop.js
+++ controllers/shop.js
@@ -161,7 +161,17 @@ exports.getInvoice = (req, res, next) => {
     res.setHeader('Content-Disposition', 'inline');
     pdfDoc.pipe(fs.createWriteStream(invoicePath))
     pdfDoc.pipe(res)
-    pdfDoc.text('hello world')
+    pdfDoc.fontSize(26).text('Invoice' , {
+      underline: true
+    })
+    pdfDoc.text('-----------------------------------------')
+    let totalPrice  = 0;
+    order.products.forEach(prod => {
+       totalPrice += prod.quantity * prod.product.price
+      pdfDoc.fontSize(14).text('productTitle:' +  prod.product.title + ' - ' + prod.quantity + ' - ' + '$' + prod.product.price)
+    })
+    pdfDoc.text('-----')
+    pdfDoc.text('Total Price $' + totalPrice)
     pdfDoc.end()
     //  fs.readFile(invoicePath, (err, data) => {
     //    if (err) {
