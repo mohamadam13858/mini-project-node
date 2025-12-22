113f771d0c780b40898eb9715b1d0352cc1c687b
mohammad <mh177148@gmail.com>
2025-12-22 16:29:18 +0330

    pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,20 +7,25 @@ const PDFDocument = require('pdfkit')
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then(products => {
-      console.log(products);
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
-    })
+  const page = +req.query.page || 1
+  let totalItems;
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts
+    return Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+  }).then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'products',
+      path: '/products' , 
+      currentPage : page , 
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems , 
+      hasPreviousPage: page > 1 , 
+      nextPage : page + 1 , 
+      previousPage : page - 1 , 
+      lastPage : Math.ceil(totalItems / ITEMS_PER_PAGE)
+    });
+  })
 };
 
 exports.getProduct = (req, res, next) => {
