42d275aec1e9f68c788d42a314e0fd44643103a9
mohammad <mh177148@gmail.com>
2025-12-26 18:26:38 +0330

    stripe

--- controllers/admin.js
+++ controllers/admin.js
@@ -191,21 +191,19 @@ exports.getProducts = (req, res, next) => {
     })
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId).then(product => {
     if (!product) {
       return next(new Error('product not found'))
     }
     fileHelper.deleteFile(product.imageUrl)
-    return   Product.deleteOne({ _id: prodId, userId: req.user._id })
+    return Product.deleteOne({ _id: prodId, userId: req.user._id })
   }).then(() => {
-      console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
-    })
+    console.log('DESTROYED PRODUCT');
+    res.status(200).json({ message: 'Success!' })
+  })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      res.status(500).json({ message: 'Deleting Product Failed.' })
     })
 };
